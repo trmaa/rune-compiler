19bb75d7159253512f842375aa25bdb661ace864
trmaa <mail@pablotrik.online>
2026-09-04 08:57:24 -0400

    better grep

--- scripts/grep.sh
+++ scripts/grep.sh
@@ -12,30 +12,35 @@ OK=0
 ERR=1
 
 help() {
+	local stat=$1
+
 	echo "Use: ./grep [opts] 'string to search'"
 	echo 'Out: The file and line number where that was found in src/'
 	echo 'Opts: -h prints this msg'
-	exit $1
+
+	exit $stat
 }
 
 main() {
 	if (( $# < 1 )); then
-		echo -e '\e[33mTo grep the base, you need a string to grep for\e[0m'
+		echo -e '\e[33mTo grep the base, you need a string to grep for\e[0m' >&2
 		help $ERR
 	fi
 
-	[[ $1 == '-h' ]] && help $OK
+	[[ "$1" == '-h' ]] && help $OK
+
+	local search="$1"
 
 	local f
 	for f in src/*; do
-		[[ -f $f ]] || continue # omit dirs
+		[[ -f "$f" ]] || continue # omit dirs
 
-		local match=$(nl -ba < $f | grep -F "$1")
+		local match=$(nl -ba -v 0 < "$f" | grep -F "$search")
 		if [[ -n $match ]]; then
-			echo $f >&2
+			echo "$f"
 			echo "$match"
 		fi
-	done
+	done | less -F
 }
 
 main "$@"
